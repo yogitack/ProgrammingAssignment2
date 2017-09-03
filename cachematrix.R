1f5291113540635daa350924aa3c19d4209b8b64
yogitack <30890098+yogitack@users.noreply.github.com>
2017-09-03 21:23:20 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Define the functions that cache the inverse of a matrix 
+##instead of computing it again
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##create a function which creates a special "Matrix" that cache its inverse
 
+makeCacheMatrix <- function(x = matrix()) { 
+  ## initialize the value of the matrix inverse to NULL
+  matrixinverse <- NULL 
+  
+  ##Set the value of the matrix
+  set <- function(y) {                      
+    x <<- y
+    ## change the value of inverse of the matrix in case the matrix was changed.
+    matrixinverse <<- NULL              
+  }
+  ## Get the value of the inverse
+  get <- function() x  
+  
+  ##Calculate the inverse of non-singular matrix using solve() function
+  setinverse <- function(solve) matrixinverse <<- solve 
+  
+  ##Get the inverse     
+  getinverse <- function() matrixinverse 
+  
+  ## Pass the value of the function makeCacheMatrix        
+  list(set = set, get = get,                    
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## Create a function which cache the inverse of the matrix 
+##( returned by makeCacheMatrix above)
+
+cacheSolve<- function(x, ...) {                 
+  matrixinverse <- x$getinverse()
+  
+  ##if the inverse exists, cacheSolve cache it.
+  if(!is.null(matrixinverse)) {                 
+    message("getting cached data - Inverse of the matrix")
+    return(matrixinverse)
+  }
+  ##if the inverse is not there, first it is calculated and then retrieved.
+  data <- x$get()                               
+  matrixinverse <- solve(data, ...)
+  x$setinverse(matrixinverse)
+  matrixinverse
 }
 
 
-## Write a short comment describing this function
+#Test the code
+testm1<- matrix(c(1,2,3,4, -3,5,2,7,6), 3,3)
+n1<- makeCacheMatrix(testm1)
+cacheSolve(n1)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+myMatrix_object <- makeCacheMatrix(m1)
+cacheSolve(myMatrix_object)
+
+#make use of set() instead of calling makeCacheMatrix again
+testm2<- matrix(c(2/3, 4/3, 5/6, 1/2), nrow = 2, ncol = 2)
+myMatrix_object$set(testm2)
+cacheSolve(myMatrix_object)
+cacheSolve(myMatrix_object)
+
+
